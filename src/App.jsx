1b756f8abc8bfe254c810b7db214f84d96929932
blanchot <lukas.blanchot@hotmail.fr>
2023-10-29 10:16:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Fragment, useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  
+  return <Fragment>
+    <h1 id="title" className='title' > yodqsssq</h1>
+    <input type="text" />
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+      Doloremque fugit quod est corrupti eligendi, quidem voluptatum, fuga odio repudiandae eos quisquam commodi tempora ipsam veritatis voluptates, id suscipit quos omnis!
+    </p>
+
+  </Fragment>
+}
+
+export default App
