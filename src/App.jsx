8ec5a96e732bd940d4f9919598c93022c12d278a
blanchot <lukas.blanchot@hotmail.fr>
2023-10-29 14:55:15 +0100

    preparation tuto suivant de Grafikart : Le hook useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,16 @@
 
-import { Fragment, useState } from 'react'
 
-const title = "Bonjour les gens";
-const style_titre = {color: 'red', backgroundColor: 'beige'}
-const showTitle = true
-const todos = [
-  'test projet',
-  'test projets 2',
-  'test projets 3'
-]
+
+
 
 
 function App() {
 
-
-  const handleClick = (e) =>{
-    //console.log(e)
-    alert("J'ai cliqué sur le titre")
-  }
   
-  return <Fragment>
   
-    {
-      showTitle ? 
-        <h1 onClick={handleClick} id="title" className='title' style={style_titre} > {title}</h1> : 
-        <p>test</p>
-    }
-    <Title color="purple" hidden id="monidcustom" className="maclassnamecustom" data-demo="demo">Rebonjour les gens</Title>
-    <input type="text" />
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Doloremque fugit quod est corrupti eligendi, quidem voluptatum, fuga odio repudiandae eos quisquam commodi tempora ipsam veritatis voluptates, id suscipit quos omnis!
-
-      test lol
-    </p>
-    <ul>
-      {todos.map(todo => (<li key={todo}>{todo}</li>))}
-    </ul>
-
-  </Fragment>
 }
 
 
-function Title({color, children, hidden, ...props }){ 
-  /*if(hidden)
-  {
-    return null
-  }*/
 
-  console.log(hidden)
-
-  const propri = {
-    id: 'monidcustom',
-    className: 'maclassenamecustome'
-  }
-
-
-  return <h1 style={{color: color}} /*{...propri}*/ {...props}>{children}</h1>
-}
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import TutoSyntaxeJsx from './tuto_Syntaxe_Jsx.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
